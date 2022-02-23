36821cb6073aa6975d23717bf2064fd06a23e15a
STING_OP <nitinkumargreat73@gmail.com>
2022-02-23 23:08:46 +0530

    Minor changes

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 $mailPhone = $_POST['usr'];
 $password = $_POST['pass'];
-$count= 0;
+$count = 0;
 $pMailErr =  $pwdErr = "";
 
-// Required field
+// Required field and Data validation
 if (isset($_POST['Login'])) {
     if (empty($mailPhone)) {
         $pMailErr = "Phone or Email required";
@@ -14,7 +14,7 @@ if (isset($_POST['Login'])) {
         if (!filter_var($mailPhone, FILTER_VALIDATE_EMAIL)) {
             $pMailErr = "Invalid Email format";
             echo $pMailErr . "<br><br>";
-            $count= $count+1;
+            $count = $count + 1;
         }
     }
     if (empty($password)) {
@@ -27,10 +27,11 @@ if (isset($_POST['Login'])) {
             $pwdErr = "Password Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters";
             echo $pwdErr . "<br><br><br>";
             echo "<a href='index.html'> Back to Login Page</a>";
-            $count= $count+1;
+            $count = $count + 1;
         }
     }
 }
+//Function for data sanitization
 function test_input($data)
 {
     $data = trim($data);
@@ -38,21 +39,21 @@ function test_input($data)
     $data = htmlspecialchars($data);
     return $data;
 }
-if($count==0){
-    $db=   mysqli_connect('localhost','root','','userlogindata');
-    if($db){
-        $sql= "select EmailPhone, pass from loginrecord WHERE EmailPhone='".$mailPhone."' and pass='".$password."'";
-        $res=mysqli_query($db,$sql);
-        if($res){
+// Login Data validation with respect to database
+if ($count == 0) {
+    $db =   mysqli_connect('localhost', 'root', '', 'userlogindata');
+    if ($db) {
+
+        $sql = "select EmailPhone, pass from loginrecord WHERE EmailPhone='" . $mailPhone . "' and  pass='" . $password . "'";
+        $res = mysqli_query($db, $sql);
+        if ($res->num_rows == 1) {
             echo "<script>window.location.href='index.html';</script>";
-        }
-        else{
+        } else {
             echo "<script>alert('You have entered Wrong Cridentials')</script>";
             echo "<script>window.location.href='index.html';</script>";
-            }
-    }
-    else{
-       die("Connection failed: " . mysqli_connect_error());
+        }
+    } else {
+        die("Connection failed: " . mysqli_connect_error());
     }
 }
 
